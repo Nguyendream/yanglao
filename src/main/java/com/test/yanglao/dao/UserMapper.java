34497b0d04c9a8b046b95dd4faa58766da0ee6a1
Nguyendream <nguyensunner@gmail.com>
2018-10-24 00:32:01 +0800

    add mybatis-generator & add UserMapper

---
+++ src/main/java/com/test/yanglao/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.test.yanglao.dao;
+
+import com.test.yanglao.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
